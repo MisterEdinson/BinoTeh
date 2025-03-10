0d94d12278e36ec86e5bd2f8fa93a79437d96bb4
Edyard Karastoyka <88448179+MisterEdinson@users.noreply.github.com>
2025-03-10 14:38:15 +0300

    commit 4

--- app/src/main/java/com/example/binoteh/MainActivity.java
+++ app/src/main/java/com/example/binoteh/MainActivity.java
@@ -12,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Log.d("!@#$%$", "222");
         Log.d("!@#$%$", "333");
+        Log.d("!@#$%$", "444");
     }
 }
